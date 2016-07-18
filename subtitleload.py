1cf234862b7ff83ed006fe9525be567d9ab94b34
Suraj Pai <surajballambat@gmail.com>
2016-07-18 13:57:57 +0530

    Add files via upload
    Convert VTT to SRT

---
+++ subtitleload.py
@@ -0,0 +1,22 @@
+import pycaption
+from pycaption import detect_format
+from pycaption import SRTWriter
+from pycaption import WebVTTReader
+import io
+
+
+# Convert from WebVTT to SRT
+# Pass the input file location as the argument to the function
+def Subtitleload(inputfile):
+    wttcaps=io.open(inputfile,"r",encoding='utf-8').read()
+    converter = pycaption.CaptionConverter()
+    #Captionset object as intermediary
+
+    converter.read(wttcaps, WebVTTReader())
+    srtcaps = io.open('youroutputfiledirectorygoeshere.srt', "w", encoding='utf-8')
+    #Write converter object into srtcaps file
+    srtcaps.write(converter.write(SRTWriter()))
+
+
+
+
